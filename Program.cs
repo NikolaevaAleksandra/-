8c3e2e90f27ada27809fd2be19a9ae0868640139
Nikolaeva Aleksandra <9724761@gmail.com>
2022-08-02 12:47:45 +0300

    Написала код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+Clear();
+string[] array =
+{
+    "n",
+    "65",
+    "hey",
+    "-957",
+    "party",
+    "1"
+};
+var result = new string[array.Length];
+var realsize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realsize] = value;
+        realsize++;
+    }
+}
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realsize));
